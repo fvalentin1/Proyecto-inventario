2c730b3e658b0cf3d101ac63105fc4a86d90e7e5
Felipe Valentín Muñoz <71657263+fvalentin1@users.noreply.github.com>
2024-11-15 15:24:55 -0300

    update home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('Has iniciado sesión correctamente!') }}
                 </div>
             </div>
         </div>
