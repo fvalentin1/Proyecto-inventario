af2c21df1560a8ba8f33cf6569d4287c052bdd59
fvalentin1 <71657263+fvalentin1@users.noreply.github.com>
2024-12-08 12:02:39 -0300

    install dashmix

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,427 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+  <title>Dashmix - Bootstrap 5 Admin Template &amp; UI Framework</title>
+
+  <meta name="description"
+    content="Dashmix - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+  <meta name="author" content="pixelcave">
+  <meta name="robots" content="noindex, nofollow">
+
+  <!-- Open Graph Meta -->
+  <meta property="og:title" content="Dashmix - Bootstrap 5 Admin Template &amp; UI Framework">
+  <meta property="og:site_name" content="Dashmix">
+  <meta property="og:description"
+    content="Dashmix - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave and published on Themeforest">
+  <meta property="og:type" content="website">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <!-- Icons -->
+  <!-- The following icons can be replaced with your own, they are used by desktop and mobile browsers -->
+  <link rel="shortcut icon" href="assets/media/favicons/favicon.png">
+  <link rel="icon" type="image/png" sizes="192x192" href="assets/media/favicons/favicon-192x192.png">
+  <link rel="apple-touch-icon" sizes="180x180" href="assets/media/favicons/apple-touch-icon-180x180.png">
+  <!-- END Icons -->
+
+  <!-- Stylesheets -->
+  <!-- Dashmix framework -->
+  <link rel="stylesheet" id="css-main" href="assets/css/dashmix.min.css">
+
+  <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
+  <link rel="stylesheet" id="css-theme" href="assets/css/themes/xplay.min.css">
+  <!-- END Stylesheets -->
+</head>
+
+<body>
+  <!-- Page Container -->
+  <!--
+      Available classes for #page-container:
+
+      GENERIC
+
+        'remember-theme'                            Remembers active color theme and dark mode between pages using localStorage when set through
+                                                    - Theme helper buttons [data-toggle="theme"],
+                                                    - Layout helper buttons [data-toggle="layout" data-action="dark_mode_[on/off/toggle]"]
+                                                    - ..and/or Dashmix.layout('dark_mode_[on/off/toggle]')
+
+      SIDEBAR & SIDE OVERLAY
+
+        'sidebar-r'                                 Right Sidebar and left Side Overlay (default is left Sidebar and right Side Overlay)
+        'sidebar-mini'                              Mini hoverable Sidebar (screen width > 991px)
+        'sidebar-o'                                 Visible Sidebar by default (screen width > 991px)
+        'sidebar-o-xs'                              Visible Sidebar by default (screen width < 992px)
+        'sidebar-dark'                              Dark themed sidebar
+
+        'side-overlay-hover'                        Hoverable Side Overlay (screen width > 991px)
+        'side-overlay-o'                            Visible Side Overlay by default
+
+        'enable-page-overlay'                       Enables a visible clickable Page Overlay (closes Side Overlay on click) when Side Overlay opens
+
+        'side-scroll'                               Enables custom scrolling on Sidebar and Side Overlay instead of native scrolling (screen width > 991px)
+
+      HEADER
+
+        ''                                          Static Header if no class is added
+        'page-header-fixed'                         Fixed Header
+
+
+      FOOTER
+
+        ''                                          Static Footer if no class is added
+        'page-footer-fixed'                         Fixed Footer (please have in mind that the footer has a specific height when is fixed)
+
+      HEADER STYLE
+
+        ''                                          Classic Header style if no class is added
+        'page-header-dark'                          Dark themed Header
+        'page-header-glass'                         Light themed Header with transparency by default
+                                                    (absolute position, perfect for light images underneath - solid light background on scroll if the Header is also set as fixed)
+        'page-header-glass page-header-dark'         Dark themed Header with transparency by default
+                                                    (absolute position, perfect for dark images underneath - solid dark background on scroll if the Header is also set as fixed)
+
+      MAIN CONTENT LAYOUT
+
+        ''                                          Full width Main Content if no class is added
+        'main-content-boxed'                        Full width Main Content with a specific maximum width (screen width > 1200px)
+        'main-content-narrow'                       Full width Main Content with a percentage width (screen width > 1200px)
+
+      DARK MODE
+
+        'sidebar-dark page-header-dark dark-mode'   Enable dark mode (light sidebar/header is not supported with dark mode)
+    -->
+  <div id="page-container"
+    class="sidebar-o sidebar-dark enable-page-overlay side-scroll page-header-fixed main-content-narrow">
+
+    <nav id="sidebar" aria-label="Main Navigation">
+      <!-- Side Header -->
+      <div class="bg-header-dark">
+        <div class="content-header bg-white-5">
+          <!-- Logo -->
+          <a class="fw-semibold text-white tracking-wide" href="index.html">
+            <span class="smini-visible">
+              D<span class="opacity-75">x</span>
+            </span>
+            <span class="smini-hidden">
+              Dash<span class="opacity-75">mix</span>
+            </span>
+          </a>
+          <!-- END Logo -->
+
+          <!-- Options -->
+          <div>
+            <!-- Toggle Sidebar Style -->
+            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+            <!-- Class Toggle, functionality initialized in Helpers.dmToggleClass() -->
+            <button type="button" class="btn btn-sm btn-alt-secondary" data-toggle="class-toggle"
+              data-target="#sidebar-style-toggler" data-class="fa-toggle-off fa-toggle-on"
+              onclick="Dashmix.layout('sidebar_style_toggle');Dashmix.layout('header_style_toggle');">
+              <i class="fa fa-toggle-off" id="sidebar-style-toggler"></i>
+            </button>
+            <!-- END Toggle Sidebar Style -->
+
+            <!-- Dark Mode -->
+            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+            <button type="button" class="btn btn-sm btn-alt-secondary" data-toggle="class-toggle"
+              data-target="#dark-mode-toggler" data-class="far fa" onclick="Dashmix.layout('dark_mode_toggle');">
+              <i class="far fa-moon" id="dark-mode-toggler"></i>
+            </button>
+            <!-- END Dark Mode -->
+
+            <!-- Close Sidebar, Visible only on mobile screens -->
+            <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+            <button type="button" class="btn btn-sm btn-alt-secondary d-lg-none" data-toggle="layout"
+              data-action="sidebar_close">
+              <i class="fa fa-times-circle"></i>
+            </button>
+            <!-- END Close Sidebar -->
+          </div>
+          <!-- END Options -->
+        </div>
+      </div>
+      <!-- END Side Header -->
+
+      <!-- Sidebar Scrolling -->
+      <div class="js-sidebar-scroll">
+        <!-- Side Navigation -->
+        <div class="content-side">
+          <ul class="nav-main">
+            <li class="nav-main-item">
+              <a class="nav-main-link" href="be_pages_dashboard.html">
+                <i class="nav-main-link-icon fa fa-location-arrow"></i>
+                <span class="nav-main-link-name">Dashboard</span>
+                <span class="nav-main-link-badge badge rounded-pill bg-primary">8</span>
+              </a>
+            </li>
+
+            <li class="nav-main-heading">Gestión Base</li>
+            <li class="nav-main-item">
+              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
+                aria-expanded="false" href="#">
+                <i class="nav-main-link-icon fa fa-border-all"></i>
+                <span class="nav-main-link-name">Salones</span>
+              </a>
+              <ul class="nav-main-submenu">
+                <li class="nav-main-item">
+                  <a class="nav-main-link" href="be_blocks_styles.html">
+                    <span class="nav-main-link-name">Administrar</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-main-item">
+              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
+                aria-expanded="false" href="#">
+                <i class="nav-main-link-icon fa fa-boxes"></i>
+                <span class="nav-main-link-name">Autos</span>
+              </a>
+              <ul class="nav-main-submenu">
+                <li class="nav-main-item">
+                  <a class="nav-main-link" href="be_widgets_tiles.html">
+                    <span class="nav-main-link-name">Administrar</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-main-item">
+              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
+                aria-expanded="false" href="#">
+                <i class="nav-main-link-icon fa fa-vector-square"></i>
+                <span class="nav-main-link-name">Noticias</span>
+              </a>
+              <ul class="nav-main-submenu">
+                <li class="nav-main-item">
+                  <a class="nav-main-link" href="be_layout_api.html">
+                    <span class="nav-main-link-name">Administrar</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-main-heading">Gestión de Usuarios</li>
+            <li class="nav-main-item">
+              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
+                aria-expanded="false" href="#">
+                <i class="nav-main-link-icon fa fa-flask"></i>
+                <span class="nav-main-link-name">Usuarios</span>
+              </a>
+              <ul class="nav-main-submenu">
+                <li class="nav-main-item">
+                  <a class="nav-main-link" href="be_ui_icons.html">
+                    <span class="nav-main-link-name">Administrar Usuarios</span>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-main-item">
+              <a class="nav-main-link nav-main-link-submenu" data-toggle="submenu" aria-haspopup="true"
+                aria-expanded="false" href="#">
+                <i class="nav-main-link-icon fa fa-grip-horizontal"></i>
+                <span class="nav-main-link-name">Roles</span>
+              </a>
+              <ul class="nav-main-submenu">
+                <li class="nav-main-item">
+                  <a class="nav-main-link" href="be_tables_styles.html">
+                    <span class="nav-main-link-name">Administrar Roles</span>
+                  </a>
+                </li>
+
+              </ul>
+            </li>
+
+
+          </ul>
+        </div>
+        <!-- END Side Navigation -->
+      </div>
+      <!-- END Sidebar Scrolling -->
+    </nav>
+    <!-- END Sidebar -->
+
+    <!-- Header -->
+    <header id="page-header">
+      <!-- Header Content -->
+      <div class="content-header">
+        <!-- Left Section -->
+        <div class="space-x-1">
+          <!-- Toggle Sidebar -->
+          <!-- Layout API, functionality initialized in Template._uiApiLayout()-->
+          <button type="button" class="btn btn-alt-secondary" data-toggle="layout" data-action="sidebar_toggle">
+            <i class="fa fa-fw fa-bars"></i>
+          </button>
+          <!-- END Toggle Sidebar -->
+
+        </div>
+        <!-- END Left Section -->
+
+        <!-- Right Section -->
+        <div class="space-x-1">
+          <!-- User Dropdown -->
+          <div class="dropdown d-inline-block">
+            <button type="button" class="btn btn-alt-secondary" id="page-header-user-dropdown" data-bs-toggle="dropdown"
+              aria-haspopup="true" aria-expanded="false">
+              <i class="fa fa-fw fa-user d-sm-none"></i>
+              <span class="d-none d-sm-inline-block">Admin</span>
+              <i class="fa fa-fw fa-angle-down opacity-50 ms-1 d-none d-sm-inline-block"></i>
+            </button>
+            <div class="dropdown-menu dropdown-menu-end p-0" aria-labelledby="page-header-user-dropdown">
+              <div class="bg-primary-dark rounded-top fw-semibold text-white text-center p-3">
+                User Options
+              </div>
+              <div class="p-2">
+                <a class="dropdown-item" href="#">
+                  <i class="far fa-fw fa-user me-1"></i> Profile
+                </a>
+
+                <div role="separator" class="dropdown-divider"></div>
+
+                <a class="dropdown-item" href="#">
+                  <i class="far fa-fw fa-building me-1"></i> Settings
+                </a>
+
+                <div role="separator" class="dropdown-divider"></div>
+                <a class="dropdown-item" href="op_auth_signin.html">
+                  <i class="far fa-fw fa-arrow-alt-circle-left me-1"></i> Sign Out
+                </a>
+              </div>
+            </div>
+          </div>
+          <!-- END User Dropdown -->
+
+        </div>
+        <!-- END Right Section -->
+      </div>
+      <!-- END Header Content -->
+
+      <!-- Header Search -->
+      <div id="page-header-search" class="overlay-header bg-header-dark">
+        <div class="bg-white-10">
+          <div class="content-header">
+            <form class="w-100" action="be_pages_generic_search.html" method="POST">
+              <div class="input-group">
+                <!-- Layout API, functionality initialized in Template._uiApiLayout() -->
+                <button type="button" class="btn btn-alt-primary" data-toggle="layout" data-action="header_search_off">
+                  <i class="fa fa-fw fa-times-circle"></i>
+                </button>
+                <input type="text" class="form-control border-0" placeholder="Search or hit ESC.."
+                  id="page-header-search-input" name="page-header-search-input">
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <!-- END Header Search -->
+
+      <!-- Header Loader -->
+      <!-- Please check out the Loaders page under Components category to see examples of showing/hiding it -->
+      <div id="page-header-loader" class="overlay-header bg-header-dark">
+        <div class="bg-white-10">
+          <div class="content-header">
+            <div class="w-100 text-center">
+              <i class="fa fa-fw fa-sun fa-spin text-white"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- END Header Loader -->
+    </header>
+    <!-- END Header -->
+
+    <!-- Main Container -->
+    <main id="main-container">
+      <!-- Hero -->
+      {{-- <div class="bg-image" style="background-image: url('assets/media/various/bg_dashboard.jpg');">
+        <div class="bg-primary-dark-op">
+          <div class="content content-full">
+            <div class="row my-3">
+              <div class="col-md-6 d-md-flex align-items-md-center">
+                <div class="py-4 py-md-0 text-center text-md-start">
+                  <h1 class="fs-2 text-white mb-2">Dashboard</h1>
+                  <h2 class="fs-lg fw-normal text-white-75 mb-0">Welcome to your overview</h2>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div> --}}
+      <!-- END Hero -->
+
+      <!-- Hero -->
+      <div class="bg-body-light">
+        <div class="content content-full">
+          <div class="d-flex flex-column flex-sm-row justify-content-sm-between align-items-sm-center">
+            <h1 class="flex-grow-1 fs-3 fw-semibold my-2 my-sm-3">Discussion</h1>
+            <nav class="flex-shrink-0 my-2 my-sm-0 ms-sm-3" aria-label="breadcrumb">
+              <ol class="breadcrumb">
+                <li class="breadcrumb-item">
+                  <a href="be_pages_forum_categories.html">Forum</a>
+                </li>
+                <li class="breadcrumb-item">
+                  <a href="be_pages_forum_topics.html">Topics</a>
+                </li>
+                <li class="breadcrumb-item active" aria-current="page">Discussion</li>
+              </ol>
+            </nav>
+          </div>
+        </div>
+      </div>
+      <!-- END Hero -->
+
+      <!-- Page Content -->
+      <div class="content">
+
+        <h2>Hello world!</h2>
+
+
+
+
+
+      </div>
+      <!-- END Page Content -->
+    </main>
+    <!-- END Main Container -->
+
+    <!-- Footer -->
+    <footer id="page-footer" class="bg-body-light">
+      <div class="content py-0">
+        <div class="row fs-sm">
+          <div class="col-sm-6 order-sm-2 mb-1 mb-sm-0 text-center text-sm-end">
+            Crafted with <i class="fa fa-heart text-danger"></i> by <a class="fw-semibold" target="_blank">pixelcave</a>
+          </div>
+          <div class="col-sm-6 order-sm-1 text-center text-sm-start">
+            <a class="fw-semibold" target="_blank">Dashmix 5.5</a> &copy; <span data-toggle="year-copy"></span>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- END Footer -->
+  </div>
+  <!-- END Page Container -->
+
+  <!--
+      Dashmix JS
+
+      Core libraries and functionality
+      webpack is putting everything together at assets/_js/main/app.js
+    -->
+  <script src="assets/js/dashmix.app.min.js"></script>
+
+  <!-- jQuery (required for jQuery Sparkline plugin) -->
+  <script src="assets/js/lib/jquery.min.js"></script>
+
+  <!-- Page JS Plugins -->
+  <script src="assets/js/plugins/jquery-sparkline/jquery.sparkline.min.js"></script>
+  <script src="assets/js/plugins/chart.js/chart.min.js"></script>
+
+  <!-- Page JS Code -->
+  <script src="assets/js/pages/be_pages_dashboard_v1.min.js"></script>
+
+  <!-- Page JS Helpers (jQuery Sparkline plugin) -->
+  <script>Dashmix.helpersOnLoad(['jq-sparkline']);</script>
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+});
+
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
