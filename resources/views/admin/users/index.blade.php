85b177038027431117f767669df1c5efcb30f2b5
Felipe Valentín Muñoz <71657263+fvalentin1@users.noreply.github.com>
2024-12-09 17:25:45 -0300

    update view users manage

--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -27,44 +27,51 @@
             </div>
         @endif
 
-        <table class="table mt-3">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Nombre</th>
-                    <th>Email</th>
-                    <th>Roles</th>
-                    <th>Acciones</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($users as $user)
-                    <tr>
-                        <td>{{ $user->id }}</td>
-                        <td>{{ $user->name }}</td>
-                        <td>{{ $user->email }}</td>
-                        <td>
-                            @foreach($user->getRoleNames() as $role)
-                                @if($role == 'admin')
-                                    <span class="badge bg-warning">{{ $role }}</span>
-                                @elseif($role == 'usuario')
-                                    <span class="badge bg-success">{{ $role }}</span>
-                                @else
-                                    <span class="badge bg-info">{{ $role }}</span>
-                                @endif
-                            @endforeach
-                        </td>
-                        <td>
-                            <a href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-primary">Editar</a>
-                            <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display: inline-block;">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger" onclick="return confirm('¿Estás seguro de que deseas eliminar este usuario?')">Eliminar</button>
-                            </form>
-                        </td>
+        <div class="table-responsive">
+            <table class="table table-bordered table-striped table-vcenter mt-3">
+                <thead>
+                    <tr class="text-center">
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Email</th>
+                        <th>Roles</th>
+                        <th>Acciones</th>
                     </tr>
-                @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                    @foreach ($users as $user)
+                        <tr class="text-center">
+                            <td>{{ $user->id }}</td>
+                            <td>{{ $user->name }}</td>
+                            <td>{{ $user->email }}</td>
+                            <td>
+                                @foreach($user->getRoleNames() as $role)
+                                    @if($role == 'admin')
+                                        <span class="badge bg-warning">{{ $role }}</span>
+                                    @elseif($role == 'usuario')
+                                        <span class="badge bg-success">{{ $role }}</span>
+                                    @else
+                                        <span class="badge bg-info">{{ $role }}</span>
+                                    @endif
+                                @endforeach
+                            </td>
+                            <td>
+                                <div>
+                                    <a type="button" href="{{ route('admin.users.edit', $user->id) }}" class="btn btn-alt-info mb-1" data-bs-toggle="tooltip"
+                                        title="Editar"><i class="fa-solid fa-pen-to-square"></i></a>
+                                    <form action="{{ route('admin.users.destroy', $user->id) }}" method="POST" style="display: inline-block;">
+                                        @csrf
+                                        @method('DELETE')
+                                        <button type="submit" class="btn btn-alt-danger mb-1" data-bs-toggle="tooltip"
+                                        title="Borrar" onclick="return confirm('¿Estás seguro de que deseas eliminar este usuario?')"><i class="fa-solid fa-trash-can"></i></button>
+                                    </form>
+                                </div>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+
     </div>
 @endsection
