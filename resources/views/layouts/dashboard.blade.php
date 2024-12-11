80cb6d695e5fecc94580e4260657a9d606ade406
Felipe Valentín Muñoz <71657263+fvalentin1@users.noreply.github.com>
2024-12-11 11:01:38 -0300

    Update dashboard.blade.php

--- resources/views/layouts/dashboard.blade.php
+++ resources/views/layouts/dashboard.blade.php
@@ -40,6 +40,7 @@
 
   <!-- You can include a specific file from css/themes/ folder to alter the default color theme of the template. eg: -->
   <link rel="stylesheet" id="css-theme" href="{{ asset('assets/css/themes/xplay.min.css') }}">
+
   <!-- END Stylesheets -->
 </head>
 
